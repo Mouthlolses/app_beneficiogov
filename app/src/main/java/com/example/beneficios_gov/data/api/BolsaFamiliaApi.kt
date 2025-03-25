7f8c928483477d930869e2ca3a4d1e244048bc0c
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-03-25 20:53:12 -0300

    initial_request

---
+++ app/src/main/java/com/example/beneficios_gov/data/api/BolsaFamiliaApi.kt
@@ -0,0 +1,13 @@
+package com.example.beneficios_gov.data.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+// interface BolsaFamiliaApi.kt com os métodos de requisição.
+/*
+interface BolsaFamiliaApi {
+    @GET("bolsa/familia/{cpf}")
+    suspend fun getBeneficio(@Path("cpf") cpf: String): Response<>
+
+}*/
