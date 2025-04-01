30a692bb2fb931e29cc0021492fe2df5a4493739
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-04-01 20:00:05 -0300

    some_changes

--- app/src/main/java/com/example/beneficios_gov/presentation/ui/consulta/ConsultationActivity.kt
+++ app/src/main/java/com/example/beneficios_gov/presentation/ui/consulta/ConsultationActivity.kt
@@ -1,12 +1,18 @@
 package com.example.beneficios_gov.presentation.ui.consulta
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.beneficios_gov.R
 import com.example.beneficios_gov.databinding.ActivityConsultationBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class ConsultationActivity : AppCompatActivity() {
 
@@ -18,6 +24,29 @@ class ConsultationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        binding.cardView.setOnClickListener {
+            val context = it.context
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_custom, null)
+            val editText = dialogView.findViewById<TextInputEditText>(R.id.editTextInput)
+
+            val openDialog = AlertDialog.Builder(context)
+                .setTitle("Informe o seu CPF")
+                .setView(dialogView)
+                .setPositiveButton("OK") { dialog, _ ->
+                    val userInput = editText.text.toString()
+                    Toast.makeText(
+                        context,
+                        "Esses é o CPF informado: $userInput",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .setNegativeButton("Fechar", null)
+                .create()
+
+            openDialog.show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
