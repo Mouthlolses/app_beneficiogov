fc5672ca526dfd8d8da3ddb5816acd89c10e4973
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-05-06 19:25:44 -0300

    test_Api

--- app/src/test/java/com/example/beneficios_gov/ExampleUnitTest.kt
+++ app/src/test/java/com/example/beneficios_gov/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.beneficios_gov
 
+import com.example.beneficios_gov.data.api.nisApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,20 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun buscarDadosNaApi() = runBlocking {
+
+        val userInput = "16315869824"
+        val userDataInput = "202501"
+
+        val response = nisApi.consultarNis(
+            nis  = userInput,
+            anoMesReferencia = userDataInput,
+            pagina = 1
+        )
+
+        print(response)
     }
 }
